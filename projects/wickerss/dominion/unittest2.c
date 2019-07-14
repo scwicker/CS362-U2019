0bf7164edb6c3907101b1afbf7620e5d8084db42
scwicker <wickerss@oregonstate.edu>
2019-07-14 18:53:41 -0400

    unit test 3

--- projects/wickerss/dominion/unittest2.c
+++ projects/wickerss/dominion/unittest2.c
@@ -25,7 +25,7 @@ int main() {
     int p, r, handCount;
     int bonus;
     int k[10] = {adventurer, council_room, feast, gardens, mine
-               , remodel, smithy, village, baron, great_hall};
+            , remodel, smithy, village, baron, great_hall};
     struct gameState G;
     int maxHandCount = 5;
     // arrays of all coppers, silvers, and golds
@@ -39,7 +39,9 @@ int main() {
         golds[i] = gold;
     }
 
-    printf ("TESTING updateCoins():\n");
+    printf ("TESTING callMinion():\n");
+
+
     for (p = 0; p < numPlayer; p++)
     {
         for (handCount = 1; handCount <= maxHandCount; handCount++)
@@ -47,7 +49,6 @@ int main() {
             for (bonus = 0; bonus <= maxBonus; bonus++)
             {
 #if (NOISY_TEST == 1)
-                printf("Test player %d with %d treasure card(s) and %d bonus.\n", p, handCount, bonus);
 #endif
                 memset(&G, 23, sizeof(struct gameState));   // clear the game state
                 r = initializeGame(numPlayer, k, seed, &G); // initialize a new game
@@ -55,28 +56,73 @@ int main() {
                 memcpy(G.hand[p], coppers, sizeof(int) * handCount); // set all the cards to copper
                 updateCoins(p, &G, bonus);
 #if (NOISY_TEST == 1)
-                printf("G.coins = %d, expected = %d\n", G.coins, handCount * 1 + bonus);
 #endif
-                assert(G.coins == handCount * 1 + bonus); // check if the number of coins is correct
 
-                memcpy(G.hand[p], silvers, sizeof(int) * handCount); // set all the cards to silver
-                updateCoins(p, &G, bonus);
-#if (NOISY_TEST == 1)
-                printf("G.coins = %d, expected = %d\n", G.coins, handCount * 2 + bonus);
-#endif
-                assert(G.coins == handCount * 2 + bonus); // check if the number of coins is correct
 
-                memcpy(G.hand[p], golds, sizeof(int) * handCount); // set all the cards to gold
-                updateCoins(p, &G, bonus);
-#if (NOISY_TEST == 1)
-                printf("G.coins = %d, expected = %d\n", G.coins, handCount * 3 + bonus);
-#endif
-                assert(G.coins == handCount * 3 + bonus); // check if the number of coins is correct
             }
         }
     }
 
-    printf("All tests passed!\n");
+    G.whoseTurn = 1;
+    int tempCoins;
+    tempCoins = G.coins;
+    callMinion(1, 0, &G, 1);
+    if (tempCoins + 2 == G.coins){
+
+        printf("PASS: option 1 added coins as expected.\n");
+    }
+
+
+
+    G.whoseTurn = 1;
+    tempCoins = G.coins;
+    callMinion(0, 1, &G, 1);
+    if (tempCoins == G.coins){
+
+        printf("PASS: option 2 skipped adding coins as expected.\n");
+    }
+
+
+
+
+    G.whoseTurn = 1;
+    G.handCount[1] = 4;
+    callMinion(0, 1, &G, 1);
+    if (G.handCount[1] == 4){
+
+        printf("PASS: option 2 discarded and drew 4 new cards\n");
+    }
+    else{
+        printf("FAILED: option 2 should have drew 4 new cards for current player\n");
+    }
+
+    G.whoseTurn = 1;
+    G.handCount[0] = 3;
+    G.deckCount[0] = 10;
+    callMinion(0, 1, &G, 1);
+    if (G.handCount[0] == 3){
+
+        printf("PASS: option 2 ignored hand count less than 4\n");
+    }
+    else{
+        printf("FAILED: Should not have redrawn for players with less than 4 cards\n");
+    }
+
+
+
+    G.whoseTurn = 1;
+    G.handCount[0] = 8;
+    G.deckCount[0] = 10;
+    callMinion(0, 1, &G, 1);
+    G.whoseTurn = 0;
+    if (numHandCards(&G) == 4){
+
+        printf("PASS: drew new cards for player over 5\n");
+    }
+    else{
+        printf("FAILED: Should  have redrawn for players with more than 5 cards\n");
+    }
+
 
     return 0;
 }
