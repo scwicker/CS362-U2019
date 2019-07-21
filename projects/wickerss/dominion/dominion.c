5c66bc34bea2929850a697859ac7539fa8f36b98
Scott Wickersham <scott.wickersham@gmail.com>
2019-07-21 11:08:15 -0700

    created assignment 4 branch

--- projects/wickerss/dominion/dominion.c
+++ projects/wickerss/dominion/dominion.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-
+//This version has bugs introduced from assignment 2
 int compare(const void* a, const void* b) {
   if (*(int*)a > *(int*)b)
     return 1;
